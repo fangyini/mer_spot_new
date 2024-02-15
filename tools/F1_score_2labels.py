ab8e5b3446f6745d4462b5176b1c65969e69ffec
Yini Fang <adia@xiaokeai.local>
2024-02-15 14:17:34 +0800

    debug expression numbers, should always be 57 and 300

--- tools/F1_score_2labels.py
+++ tools/F1_score_2labels.py
@@ -374,15 +374,8 @@ def main_threshold(path, dataset, annotation, version, label_frequency, start_th
             # M1： Number of macro-expressions
             # M2： Number of micro-expressions
             if dataset == 'cas(me)^2' or dataset == 'cas(me)^2_merge':
-                if annotation == 'casme2_annotation_new.csv':
-                    M1= 279 #282
-                    M2 = 57 # 84
-                elif annotation == 'casme2_annotation_357.csv':
                     M1 = 300
                     M2 = 57
-                else:
-                    print(annotation)
-                    quit()
 
             else:
                 M1 = 340
